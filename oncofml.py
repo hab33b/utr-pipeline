a61d49b5cdc6d251e72ea6e0f161426de6448aa7
Habeeb Jimoh <71016968+hab33b@users.noreply.github.com>
2022-04-26 18:36:25 -0700

    Add files via upload

--- oncofml.py
+++ oncofml.py
@@ -17,33 +17,45 @@ def pool_samples():
     return pd.concat(dfs, axis=0)
 
 
-def main():
-    variants = pool_samples()
+def liftover(variants):
     lo = LiftOver("hg38", "hg19")
 
+    # LIFTOVER
     # 0-index the start_position for LiftOver
     variants["Start_Position"] = variants["Start_Position"].apply(lambda x: x - 1)
 
-    # convert positions from hg38 to hg19
+    # convert positions from hg38 to hg19 using liftover
     variants["Start_Position"] = variants[["Chromosome", "Start_Position", "End_Position"]].apply(
         lambda x: lo.convert_coordinate(x[0], x[1])[0][1] if lo.convert_coordinate(x[0], x[1]) else None, axis=1)
     variants["End_Position"] = variants[["Chromosome", "Start_Position", "End_Position"]].apply(
         lambda x: lo.convert_coordinate(x[0], x[2])[0][1] if lo.convert_coordinate(x[0], x[2]) else None, axis=1)
 
-    # delete failed conversions
+    # delete failed liftover conversions
     variants = variants[(variants["Start_Position"].isna() | variants["Start_Position"].isna()) == False]
 
+    # return position to 1-index
+    variants["Start_Position"] = variants["Start_Position"].apply(lambda x: int(x + 1))
+
+    return variants
+
+
+def oncofile(variants):
     # subset & rename for oncodrivefml mutation file
     mut = variants[["Chromosome", "Start_Position", "Reference_Allele", "Tumor_Seq_Allele2", "Tumor_Sample_Barcode"]]
-    mut["Start_Position"] = mut["Start_Position"].astype(int)
     mut.columns = ["CHROMOSOME", "POSITION", "REF", "ALT", "SAMPLE"]
 
     # utr3 & utr5
     utr3 = mut[variants["Variant_Classification"] == "3'Flank"]
     utr5 = mut[variants["Variant_Classification"] == "5'Flank"]
 
-    utr3.to_csv("all_utr3", sep="\t", index=False)
-    utr5.to_csv("all_utr5", sep="\t", index=False)
+    utr3.to_csv("utr3_all", sep="\t", index=False)
+    utr5.to_csv("utr5_all", sep="\t", index=False)
+
+
+def main():
+    variants = pool_samples()
+    variants = liftover(variants)
+    oncofile(variants)
 
 
 if __name__ == "__main__":
